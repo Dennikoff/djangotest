13d8d3bbf619d7dc641f5191f77587ae1e190ea6
hda <hda@hpc-park.ru>
2024-02-27 15:26:23 +0000

    add custom converter

--- src/women/urls.py
+++ src/women/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, register_converter
 
 from . import views
+from . import converters
+
+register_converter(converters.FourDigitConverter, 'year4')
 
 urlpatterns = [
     path('', views.index),
     path('cat/<int:cat_id>/', views.cat),
     path('cat/<slug:cat_slug>/', views.catSlug),
-    re_path(r'^archive/(?P<year>\d{4})/', views.archive),
+    path("archive/<year4:year>/", views.archive),
     re_path(r'(?P<path>.*)', views.notFound),
 ]
