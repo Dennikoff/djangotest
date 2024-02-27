7df71e7b29e8248d489fc1dc57206242e2b0a40b
hda <hda@hpc-park.ru>
2024-02-27 14:52:23 +0000

    make urls in women section

--- src/djangotest/urls.py
+++ src/djangotest/urls.py
@@ -15,12 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from women.views import index as womenIndex
-from women.views import cat 
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('women/', womenIndex),
-    path('cat/', cat),
+    path('women/', include('women.urls')),
 ]
